1995c13c043911e70414c603ffd1f4a6f7a8e2d3
Alex Ruperto <alex.ruperto@vanderlande.com>
2024-02-20 02:35:24 -0500

    Added the longest substring without repeats solution.

--- Algorithms/3_LongestSubstringWithoutRepeatingCharacters.py
+++ Algorithms/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,7 +1,7 @@
 '''
 Given a string s, find the length of the longest 
 substring
- without repeating characters.
+without repeating characters.
 
  
 
@@ -28,3 +28,36 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 '''
+
+class Solution:
+    def lengthOfLongestSubstring(self, string: str) -> (str, int): # return a string and an int
+        # store the position of characters
+        characterIndexMap = {}
+        start = maxLength = 0
+        longestSubstring = ""
+
+        for i, character in enumerate(string): # for each character in the string 
+            # If character is already in dictionary and its index is within the current substring
+            if character in characterIndexMap and characterIndexMap[character] >= start:
+                # update start to the next character after the last occurence.
+                start = characterIndexMap[character] + 1
+
+            # update the last position of the character
+            characterIndexMap[character] = i
+
+            # update maxLength and longestSubstring if the current substring is longer than maxLength
+            if i - start + 1 > maxLength:
+                maxLength = i - start + 1
+                longestSubstring=string[start:i+1]
+
+        return longestSubstring, maxLength
+
+
+
+if __name__ == "__main__":
+    solution = Solution() # init a variable called solution and set it to the Solution class.
+    s = "abcabcbb" 
+
+    substr, length = solution.lengthOfLongestSubstring(s)
+
+    print(f"Longest substring without repeating characters: '{substr}' with length {length}")
